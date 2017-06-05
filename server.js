1b18626d7f15d622e5a830aec3a5c2e08ba461f7
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2017-06-06 09:34:31 +1000

    Update Server application to accept Airport query
    Update the Server application server to use the Flight API instance's
    function `airports` to query the Locomotive code task API with its own
    query parameter
    
    == Notes:
    - Fix some style issues with missing space before brackets after
      declaring an anonymous functions
    
    - Add tests to ensure the new `airports` endpoint responds as expected
    
    == References:
    - [airbnb/javascript: JavaScript Style Guide]
      (https://github.com/airbnb/javascript)

--- server.js
+++ server.js
@@ -8,7 +8,16 @@ app.use(express.static('public'));
 
 // Lists all available airlines from the Flight API.
 app.get('/airlines', function (req, res) {
-  api.airlines(function(error, data) {
+  api.airlines(function (error, data) {
+    res.status(200).json(data);
+  });
+})
+
+// Lists all matching airports from the Flight API.
+app.get('/airports', function (req, res) {
+  let query = req.query.q;
+
+  api.airports(query, function (error, data) {
     res.status(200).json(data);
   });
 })
--- spec/serverSpec.js
+++ spec/serverSpec.js
@@ -7,6 +7,7 @@ const server = require('../server.js');
 const baseURL = 'http://localhost:3000/';
 const airlinesURL = url.resolve(baseURL, 'airlines');
 
+
 describe('Application Server', function () {
   describe('GET /', function () {
     it('returns status code 200', function (done) {
@@ -42,6 +43,90 @@ describe('Application Server', function () {
         const data = JSON.parse(body);
         expect(data).toEqual(jasmine.any(Array));
         done();
+      });
+    });
+  });
+
+  describe('GET /airports', function () {
+    beforeEach(function () {
+      // http://node.locomote.com/code-task/airports?q=Melbourne
+      const melbourneAirportData = [
+        { airportCode: "MLB", airportName: "Melbourne International Arpt" },
+        { airportCode: "MEL", airportName: "Tullamarine Arpt" }
+      ];
+
+      nock('http://node.locomote.com')
+        .get('/code-task/airports?q=Melbourne')
+        .reply(200, melbourneAirportData);
+
+      // http://node.locomote.com/code-task/airports?q=Sydney
+      const sydneyAirportData = [
+        { airportCode: "SYD", airportName: "Kingsford Smith" },
+        { airportCode: "YQY", airportName: "Sydney Airport" }
+      ];
+
+      nock('http://node.locomote.com')
+        .get('/code-task/airports?q=Sydney')
+        .reply(200, sydneyAirportData);
+    });
+
+    describe('when querying Melbourne airports', function () {
+      const melboruneAirportURL = url.resolve(baseURL, 'airports?q=Melbourne');
+
+      it('returns status code 200', function (done) {
+        request.get(melboruneAirportURL, function (error, response, body) {
+          expect(response.statusCode).toBe(200);
+          done();
+        });
+      });
+
+      it('returns an array of values', function (done) {
+        request.get(melboruneAirportURL, function (error, response, body) {
+          const data = JSON.parse(body);
+          expect(data).toEqual(jasmine.any(Array));
+          done();
+        });
+      });
+
+      it('returns expected first airport', function (done) {
+        request.get(melboruneAirportURL, function (error, response, body) {
+          const data = JSON.parse(body);
+          const airport = data[0];
+
+          expect(airport.airportCode).toEqual("MLB");
+          expect(airport.airportName).toEqual("Melbourne International Arpt");
+          done();
+        });
+      });
+    });
+
+    describe('when querying Sydney airports', function () {
+      const sydneyAirportURL = url.resolve(baseURL, 'airports?q=Sydney');
+
+      it('returns status code 200', function (done) {
+        request.get(sydneyAirportURL, function (error, response, body) {
+          expect(response.statusCode).toBe(200);
+          done();
+        });
+      });
+
+      it('returns an array of values', function (done) {
+        request.get(sydneyAirportURL, function (error, response, body) {
+          const data = JSON.parse(body);
+          expect(data).toEqual(jasmine.any(Array));
+          done();
+        });
+      });
+
+      it('returns expected last airport', function (done) {
+        request.get(sydneyAirportURL, function (error, response, body) {
+          const data = JSON.parse(body);
+          const airport = data[data.length - 1];
+
+          expect(airport.airportCode).toEqual("YQY");
+          expect(airport.airportName).toEqual("Sydney Airport");
+          done();
+        });
       });
     });
   });
