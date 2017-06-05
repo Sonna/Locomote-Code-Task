18c5b18ab6eadb364cd285ea47d87cdf8be79ccf
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2017-06-05 15:18:39 +1000

    Update back-end Server application with FlightAPI
    Update the back-end server application code to utilise the new Flight
    API to keep the endpoints and external APIs as separate concerns. The
    application will now provide a list of airlines at the `airlines` route
    from the external Locomotive code task API.
    
    == Notes:
    - Update server specification tests to stub out remote request calls
    
    - Fix inconsistent JS style issues; e.g.
      + Spaces between anonymous functions and brackets,
      + Order of imported libraries
      + Use of `var` over `const` or `let`
    
    == References:
    - [airbnb/javascript: JavaScript Style Guide]
      (https://github.com/airbnb/javascript)

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const FlightAPI = require('./lib/services/FlightAPI');
+
 const app = express();
+const api = new FlightAPI();
 
 app.use(express.static('public'));
 
 // Lists all available airlines from the Flight API.
 app.get('/airlines', function (req, res) {
-  var airlines = [];
-  res.status(200).send(airlines);
+  api.airlines(function(error, data) {
+    res.status(200).json(data);
+  });
 })
 
 app.listen(3000);
--- spec/serverSpec.js
+++ spec/serverSpec.js
@@ -1,32 +1,45 @@
+const nock = require('nock');
+const request = require('request');
 const url = require('url');
 
-var request = require('request');
-var server = require('../server.js');
+const server = require('../server.js');
 
 const baseURL = 'http://localhost:3000/';
 const airlinesURL = url.resolve(baseURL, 'airlines');
 
-describe('Application Server', function() {
-  describe('GET /', function() {
-    it('returns status code 200', function(done) {
-      request.get(baseURL, function(error, response, body) {
+describe('Application Server', function () {
+  describe('GET /', function () {
+    it('returns status code 200', function (done) {
+      request.get(baseURL, function (error, response, body) {
         expect(response.statusCode).toBe(200);
         done();
       });
     });
   });
 
-  describe('GET /airlines', function() {
-    it('returns status code 200', function(done) {
-      request.get(airlinesURL, function(error, response, body) {
+  describe('GET /airlines', function () {
+    beforeEach(function () {
+      const airlinesData = [
+        { code: "BF", name: "BarFoo" },
+        { code: "BB", name: "BazBar" }
+      ];
+
+      // Mock external request response
+      nock('http://node.locomote.com')
+        .get('/code-task/airlines')
+        .reply(200, airlinesData);
+    });
+
+    it('returns status code 200', function (done) {
+      request.get(airlinesURL, function (error, response, body) {
         expect(response.statusCode).toBe(200);
         done();
       });
     });
 
-    it('returns an array of values', function(done) {
-      request.get(airlinesURL, function(error, response, body) {
-        let data = JSON.parse(response.body);
+    it('returns an array of values', function (done) {
+      request.get(airlinesURL, function (error, response, body) {
+        const data = JSON.parse(body);
         expect(data).toEqual(jasmine.any(Array));
         done();
       });
