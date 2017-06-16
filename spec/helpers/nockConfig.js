3324ee869105f85f8c6ad851debc3e4299e3ecb0
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2017-06-16 11:24:50 +1000

    Update Nock setting to disable remote HTTP request
    Add a new test helper script to disable all remote HTTP requests using
    the Nock test library, this is to ensure that during tests that no
    _real_ HTTP requests are being made to remote servers and that the tests
    are self-contained; i.e. will work offline and without external
    resources.
    
    == References:
    - [node-nock/nock: HTTP mocking and expectations library]
      (https://github.com/node-nock/nock/#enabledisable-real-http-request)

---
+++ spec/helpers/nockConfig.js
@@ -0,0 +1,7 @@
+const nock = require('nock');
+
+// - [node-nock/nock: HTTP mocking and expectations library]
+//   (https://github.com/node-nock/nock/)
+beforeAll(function () {
+  nock.disableNetConnect();
+});
