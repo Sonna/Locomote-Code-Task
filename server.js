e53dbd62f66d5916060bc84b05affe5595f75972
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2017-11-17 15:41:53 +1100

    Refactor File serving into its own file
    Refactor the serving of files from the Node server functionality into
    its own script file, with some configuration options for changing the
    root folder it serves from; e.g. `./public` directory by default).

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const http = require('http');
-const fs = require('fs');
-const path = require('path');
 const url = require('url');
 const FlightAPI = require('./lib/services/FlightAPI');
+const FileServer = require('./lib/services/FileServer');
 
 const api = new FlightAPI();
+const fileServer = new FileServer();
 
 // Airlines Controller
 // Lists all available airlines from the Flight API.
@@ -49,56 +49,6 @@ const routes = {
   '/search': searchCreate
 }
 
-function serveFile(request, response) {
-  const publicFolder = './public';
-  let filePath = publicFolder + request.url;
-
-  if (request.url == '/') {
-    filePath = publicFolder + '/index.html';
-  }
-
-  const extname = String(path.extname(filePath)).toLowerCase();
-  let contentType = 'text/html';
-  const mimeTypes = {
-    '.html': 'text/html',
-    '.js': 'text/javascript',
-    '.css': 'text/css',
-    '.json': 'application/json',
-    // '.png': 'image/png',
-    // '.jpg': 'image/jpg',
-    // '.gif': 'image/gif',
-    // '.wav': 'audio/wav',
-    // '.mp4': 'video/mp4',
-    // '.woff': 'application/font-woff',
-    // '.ttf': 'application/font-ttf',
-    // '.eot': 'application/vnd.ms-fontobject',
-    // '.otf': 'application/font-otf',
-    // '.svg': 'application/image/svg+xml'
-  };
-
-  contentType = mimeTypes[extname] || 'application/octet-stream';
-
-  fs.readFile(filePath, function(error, content) {
-    if (error) {
-      if(error.code == 'ENOENT'){
-        fs.readFile(publicFolder + '/404.html', function(error, content) {
-          response.writeHead(200, { 'Content-Type': contentType });
-          response.end(content, 'utf-8');
-        });
-      }
-      else {
-        response.writeHead(500);
-        response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
-        response.end();
-      }
-    }
-    else {
-      response.writeHead(200, { 'Content-Type': contentType });
-      response.end(content, 'utf-8');
-    }
-  });
-}
-
 function router(request, response) {
   const req = url.parse(request.url, true);
   const service = routes[req.pathname];
@@ -106,7 +56,7 @@ function router(request, response) {
   if(service != null) {
     service(request, response);
   } else {
-    serveFile(request, response);
+    fileServer.handle(request, response);
   }
 }
 
