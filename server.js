46af35bbd6c32d60054d13d0ba9ff42ead5e2899
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2017-11-17 15:12:59 +1100

    Refactor serving files into its own function
    Refactor the serving files from the server into its own function
    independent of the HTTP server creation and routing of of paths to
    functions, in order to better encapsulate its body of work.

--- server.js
+++ server.js
@@ -49,6 +49,56 @@ const routes = {
   '/search': searchCreate
 }
 
+function serveFile(request, response) {
+  const publicFolder = './public';
+  let filePath = publicFolder + request.url;
+
+  if (request.url == '/') {
+    filePath = publicFolder + '/index.html';
+  }
+
+  const extname = String(path.extname(filePath)).toLowerCase();
+  let contentType = 'text/html';
+  const mimeTypes = {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.json': 'application/json',
+    // '.png': 'image/png',
+    // '.jpg': 'image/jpg',
+    // '.gif': 'image/gif',
+    // '.wav': 'audio/wav',
+    // '.mp4': 'video/mp4',
+    // '.woff': 'application/font-woff',
+    // '.ttf': 'application/font-ttf',
+    // '.eot': 'application/vnd.ms-fontobject',
+    // '.otf': 'application/font-otf',
+    // '.svg': 'application/image/svg+xml'
+  };
+
+  contentType = mimeTypes[extname] || 'application/octet-stream';
+
+  fs.readFile(filePath, function(error, content) {
+    if (error) {
+      if(error.code == 'ENOENT'){
+        fs.readFile(publicFolder + '/404.html', function(error, content) {
+          response.writeHead(200, { 'Content-Type': contentType });
+          response.end(content, 'utf-8');
+        });
+      }
+      else {
+        response.writeHead(500);
+        response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+        response.end();
+      }
+    }
+    else {
+      response.writeHead(200, { 'Content-Type': contentType });
+      response.end(content, 'utf-8');
+    }
+  });
+}
+
 http.createServer(function (request, response) {
   const req = url.parse(request.url, true);
   const service = routes[req.pathname];
@@ -56,53 +106,7 @@ http.createServer(function (request, response) {
   if(service != null) {
     service(request, response);
   } else {
-    const publicFolder = './public';
-    let filePath = publicFolder + request.url;
-
-    if (request.url == '/') {
-      filePath = publicFolder + '/index.html';
-    }
-
-    const extname = String(path.extname(filePath)).toLowerCase();
-    let contentType = 'text/html';
-    const mimeTypes = {
-      '.html': 'text/html',
-      '.js': 'text/javascript',
-      '.css': 'text/css',
-      '.json': 'application/json',
-      // '.png': 'image/png',
-      // '.jpg': 'image/jpg',
-      // '.gif': 'image/gif',
-      // '.wav': 'audio/wav',
-      // '.mp4': 'video/mp4',
-      // '.woff': 'application/font-woff',
-      // '.ttf': 'application/font-ttf',
-      // '.eot': 'application/vnd.ms-fontobject',
-      // '.otf': 'application/font-otf',
-      // '.svg': 'application/image/svg+xml'
-    };
-
-    contentType = mimeTypes[extname] || 'application/octet-stream';
-
-    fs.readFile(filePath, function(error, content) {
-      if (error) {
-        if(error.code == 'ENOENT'){
-          fs.readFile(publicFolder + '/404.html', function(error, content) {
-            response.writeHead(200, { 'Content-Type': contentType });
-            response.end(content, 'utf-8');
-          });
-        }
-        else {
-          response.writeHead(500);
-          response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
-          response.end();
-        }
-      }
-      else {
-        response.writeHead(200, { 'Content-Type': contentType });
-        response.end(content, 'utf-8');
-      }
-    });
+    serveFile(request, response);
   }
 }).listen(3000);
 
