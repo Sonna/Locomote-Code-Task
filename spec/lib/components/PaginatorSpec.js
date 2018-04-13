4f8a3b4f659bd48cdc1ce1bcd6fb72d5825947ed
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-04-13 12:26:28 +1000

    Add Paginator to Application Root Component
    Add and Update Paginator to work with the Application Root Component,
    rather than working in isolation. This Paginator helps reduce the
    information overload for each Tab and reduces results visually to 10
    each page within each Tab.
    
    == Notes:
    - This commit is of work done over 6 months over left unfinished due to
      my emotional/mental state

--- spec/lib/components/PaginatorSpec.js
+++ spec/lib/components/PaginatorSpec.js
@@ -37,21 +37,23 @@ describe('Paginator component', function () {
     });
 
     it('render with 2 pages, 1 per page, first page', function (done) {
-      const firstEl = 'hello';
-      const lastEl = 'world';
+      const firstEl = {
+        render: function() { return document.createTextNode('hello'); }};
+      const lastEl = {
+        render: function() { return document.createTextNode('world'); }};
       const twoPageSubject = new describedClass({
         per: 1,
         content: [firstEl, lastEl]
       });
       expect(twoPageSubject.render().outerHTML).toEqual(
         '<section class="pagination">' +
-          '<div class="page" style="display: block;">hello</div>' +
-          '<div class="page" style="display: none;">world</div>' +
+          '<div class="page" data-paginator-index="page-0" style="display: block;">hello</div>' +
+          '<div class="page" data-paginator-index="page-1" style="display: none;">world</div>' +
           '<div class="paginator">' +
-            '<a href="#" class="active">1</a>' +
-            '<a href="#">2</a>' +
-            '<a href="#">›</a>' +
-            '<a href="#">»</a>' +
+            '<button class="page-link active" data-paginator-index="0">1</button>' +
+            '<button class="page-link" data-paginator-index="1">2</button>' +
+            '<button class="page-link" data-paginator-index="1">›</button>' +
+            '<button class="page-link" data-paginator-index="1">»</button>' +
           '</div>' +
         '</section>'
       );
@@ -59,8 +61,10 @@ describe('Paginator component', function () {
     });
 
     it('render with 2 pages, 1 per page, last page', function (done) {
-      const firstEl = 'hello';
-      const lastEl = 'world';
+      const firstEl = {
+        render: function() { return document.createTextNode('hello'); }};
+      const lastEl = {
+        render: function() { return document.createTextNode('world'); }};
       const twoPageSubject = new describedClass({
         per: 1,
         index: 1,
@@ -68,13 +72,13 @@ describe('Paginator component', function () {
       });
       expect(twoPageSubject.render().outerHTML).toEqual(
         '<section class="pagination">' +
-          '<div class="page" style="display: none;">hello</div>' +
-          '<div class="page" style="display: block;">world</div>' +
+          '<div class="page" data-paginator-index="page-0" style="display: none;">hello</div>' +
+          '<div class="page" data-paginator-index="page-1" style="display: block;">world</div>' +
           '<div class="paginator">' +
-            '<a href="#">«</a>' +
-            '<a href="#">‹</a>' +
-            '<a href="#">1</a>' +
-            '<a href="#" class="active">2</a>' +
+            '<button class="page-link" data-paginator-index="0">«</button>' +
+            '<button class="page-link" data-paginator-index="0">‹</button>' +
+            '<button class="page-link" data-paginator-index="0">1</button>' +
+            '<button class="page-link active" data-paginator-index="1">2</button>' +
           '</div>' +
         '</section>'
       );
@@ -82,9 +86,12 @@ describe('Paginator component', function () {
     });
 
     it('render with 3 pages in middle of pages', function (done) {
-      const firstEl = 'hello';
-      const middleEl = 'foobar';
-      const lastEl = 'world';
+      const firstEl = {
+        render: function() { return document.createTextNode('hello'); }};
+      const middleEl = {
+        render: function() { return document.createTextNode('foobar'); }};
+      const lastEl = {
+        render: function() { return document.createTextNode('world'); }};
       const twoPageSubject = new describedClass({
         per: 1,
         index: 1,
@@ -92,17 +99,17 @@ describe('Paginator component', function () {
       });
       expect(twoPageSubject.render().outerHTML).toEqual(
         '<section class="pagination">' +
-          '<div class="page" style="display: none;">hello</div>' +
-          '<div class="page" style="display: block;">foobar</div>' +
-          '<div class="page" style="display: none;">world</div>' +
+          '<div class="page" data-paginator-index="page-0" style="display: none;">hello</div>' +
+          '<div class="page" data-paginator-index="page-1" style="display: block;">foobar</div>' +
+          '<div class="page" data-paginator-index="page-2" style="display: none;">world</div>' +
           '<div class="paginator">' +
-            '<a href="#">«</a>' +
-            '<a href="#">‹</a>' +
-            '<a href="#">1</a>' +
-            '<a href="#" class="active">2</a>' +
-            '<a href="#">3</a>' +
-            '<a href="#">›</a>' +
-            '<a href="#">»</a>' +
+            '<button class="page-link" data-paginator-index="0">«</button>' +
+            '<button class="page-link" data-paginator-index="0">‹</button>' +
+            '<button class="page-link" data-paginator-index="0">1</button>' +
+            '<button class="page-link active" data-paginator-index="1">2</button>' +
+            '<button class="page-link" data-paginator-index="2">3</button>' +
+            '<button class="page-link" data-paginator-index="2">›</button>' +
+            '<button class="page-link" data-paginator-index="2">»</button>' +
           '</div>' +
         '</section>'
       );
